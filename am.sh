bef09a1933d81806c97feafa23d25d23b0d520ed
Mark S. Petrovic <mspetrovic@gmail.com>
2013-09-01 18:08:24 -0700

    add -R -f options to vim when editing

--- am.sh
+++ am.sh
@@ -14,7 +14,7 @@ DECRYPTED_FILE=$(mktemp /tmp/am.XXXX)
 source $HOME/.bucketrc
 ./s3file.py --get --bucket ${BUCKET} --key ${KEY} --file ${DOWNLOAD_FILE}
 gpg --yes --output ${DECRYPTED_FILE} --decrypt ${DOWNLOAD_FILE}
-vi -n ${DECRYPTED_FILE}
+vi -R -n -f ${DECRYPTED_FILE}
 gpg --recipient "${RECIPIENT}" --encrypt ${DECRYPTED_FILE}
 ./s3file.py --put --bucket ${BUCKET} --key ${KEY} --file "${DECRYPTED_FILE}.gpg"
 cleanup
